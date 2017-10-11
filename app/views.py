d6817c4916c2c3f83097470be1635da55e1e4f59
Stephen Muthama <muthamass@gmail.com>
2017-10-11 16:10:00 +0300

    adds secret key

--- app/views.py
+++ app/views.py
@@ -1,15 +1,22 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, request, flash
 
 from app import app
+from app.forms import RegistrationForm
+
+app.secret_key = 'This is my secret_key'
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/register')
-def register():
-    return render_template("signup.html")
-
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        flash('Registration Successfull.Proceed to Login')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
