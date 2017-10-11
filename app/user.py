341ada4fe654908c3ee6add2c9236c5319e22ef2
Stephen Muthama <muthamass@gmail.com>
2017-10-11 21:48:28 +0300

    Insers user to dictionary

--- app/user.py
+++ app/user.py
@@ -18,28 +18,30 @@ class User():
         self.cpassword = cpassword
 
     def create(self, email, username, password, cpassword):
-        
+        """ Method for creating a user"""
         if email != '' and username != '' and password != '' and cpassword != '':
             newmail = email.strip()
             newname = username.strip()
             newpass = password.strip()
             newcpass = cpassword.strip()
             if (
-                len(newmail) == len(email) and
-                len(newname) == len(username) and
-                len(newpass) == len(password) and
-                len(newcpass) == len(cpassword)
+                    len(newmail) == len(email) and
+                    len(newname) == len(username) and
+                    len(newpass) == len(password) and
+                    len(newcpass) == len(cpassword)
             ):
                 regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)"
                 if re.search(regex, email):
-                    nameLength = len(username)
-                    if nameLength >= 6 and namelength <= 25:
+                    namelength = len(username)
+                    if namelength >= 6 and namelength <= 25:
                         passlength = len(password)
-                        if passLength >= 6 and passlength <= 25:
+                        if passlength >= 6 and passlength <= 25:
                             if email not in USERS.keys():
                                 if password == cpassword:
-
-                                return 7   
+                                    USERS[email] = [username, password]
+                                    result = email in USERS.keys()
+                                    return result
+                                return 7
                             return 6
                         return 5
                     return 4    
