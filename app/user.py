dbc83db35205500d86434913b8a2ecb4f2350e85
Stephen Muthama <muthamass@gmail.com>
2017-10-11 23:57:12 +0300

    updates code readability

--- app/user.py
+++ app/user.py
@@ -16,37 +16,83 @@ class User():
         self.email = email
         self.password = password
 
-    def create(self, email, username, password, cpassword):
-        """ Method for creating a user"""
-        if email != '' and username != '' and password != '' and cpassword != '':
+    def check_email(self, email):
+        if email != '':
             newmail = email.strip()
-            newname = username.strip()
-            newpass = password.strip()
-            newcpass = cpassword.strip()
-            if (
-                    len(newmail) == len(email) and
-                    len(newname) == len(username) and
-                    len(newpass) == len(password) and
-                    len(newcpass) == len(cpassword)
-            ):
+            if len(newmail) == len(email):
                 emaillength = len(email)
                 if emaillength >= 6 and emaillength <= 50:
                     regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)"
                     if re.search(regex, email):
-                        namelength = len(username)
-                        if namelength >= 6 and namelength <= 25:
-                            passlength = len(password)
-                            if passlength >= 6 and passlength <= 25:
-                                if email not in USERS.keys():
-                                    if password == cpassword:
-                                        USERS[email] = [username, password]
-                                        result = email in USERS.keys()
-                                        return result
-                                    return 8
-                                return 7
-                            return 6
-                        return 5
-                    return 4
-                return 3
-            return 2
+                        if email not in USERS.keys():
+                            return True
+                    message = "Enter a valid email"
+                    return message
+                message = "email length too big"
+                return message
+            message = "Remove Trailing whitespaces from email"
+            return message
+        message = "Email should not be blank"
+        return message
+
+    def check_password(self, password, cpassword):
+        if password != '':
+            newpass = password.strip()
+            if len(newpass) == len(password):
+                passlength = len(password)
+                if passlength >= 6 and passlength <= 25:
+                    if password == cpassword:
+                        return True
+                    message "confirm password coreectly"
+                    return message
+                message = "min 6 and max 25 characters"
+                return message
+            message = "Remove Trailing whitespaces from password"
+            return message
+        message = "Password should not be blank"
+        return message
+
+    def check_username(self, username):
+        if username != '':
+            newname = username.strip()
+            if len(newname) == len(username):
+                namelength = len(username)
+                if namelength >= 6 and namelength <= 25:
+                    return True
+                message = "min 6 and max 25 characters"
+                return message
+            message = return "Remove Trailing whitespaces from username"
+            return message
+        message = return "Username should not be blank"
+        return message
+
+    def create(self, email, username, password, cpassword):
+        """ Method for creating a user"""
+        check_email = self.check_email(email)
+        if check_email is True:
+            check_username = self.check_username(username)
+            if check_username is True:
+                check_password = self.check_password(password, cpassword)
+                if check_password is True:
+                    USERS[email] = [username, password]
+                    result = email in USERS.keys()
+                    if result is True:
+                        message = "Registration Successfull"
+                        return message
+                    message = "Failed to create user"
+                    return message
+                return check_password
+            return check_username
+        return check_email
+                
+
+    def check_email_in_db(self, email):
+        """ Method to check if email is in the db"""
+        emails = USERS.keys()
+        if email in emails:
+            return email
         return 1
+
+    def check_matching_passwords()
+
+
