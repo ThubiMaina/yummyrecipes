71e0d0894735f687fd8ddef98bd0392469567bfd
Stephen Muthama <muthamass@gmail.com>
2017-10-11 20:22:21 +0300

    tests if input fields are empty

--- tests/test_user.py
+++ tests/test_user.py
@@ -23,6 +23,29 @@ class TestUser(unittest.TestCase):
         """ method to test if input fields are empty"""
         result = self.new_user.create('', '', '', '')
         self.assertEqual(1, result, "Input fields empty")
+    
+    def test_email_is_empty(self):
+        """ method to test if email field is empty"""
+        result = self.new_user.create('', 'Peter', 'patpass', 'patpass')
+        self.assertEqual(1, result, "Input fields empty")
+
+    def test_username_is_empty(self):
+        """ method to test if username field is empty"""
+        result = self.new_user.create('maua@mail.com', '', 'plutos', 'plutos')
+        self.assertEqual(1, result, "Username empty")
+    
+    def test_password_is_empty(self):
+        """ method to test if password field is empty"""
+        result = self.new_user.create('maua@mail.com', 'Earths', '', 'plutos')
+        self.assertEqual(1, result, "Password empty")
+    
+    def test_cpassword_is_empty(self):
+        """ method to test if cpassword field is empty"""
+        result = self.new_user.create('maua@mail.com', 'Earths', 'plutos', '')
+        self.assertEqual(1, result, "Confirm Password empty")
+    
+    
+   
 
     def test_create_user(self):
         """ method to test for success in creating user account"""
