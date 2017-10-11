edc67a02a5a8eff4ff2d25a87f06e763765c5f9a
Stephen Muthama <muthamass@gmail.com>
2017-10-12 01:06:46 +0300

    updates resister method

--- app/views.py
+++ app/views.py
@@ -2,21 +2,36 @@ from flask import render_template, url_for, redirect, request, flash
 
 from app import app
 from app.forms import RegistrationForm
+from app.user import User
+
+NEWUSER = User()
 
 app.secret_key = 'This is my secret_key'
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/login')
 def login():
     return render_template("login.html")
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
+    error = None
     if request.method == 'POST' and form.validate():
-        flash('Registration Successfull.Proceed to Login')
-        return redirect(url_for('login'))
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        cpassword = request.form['cpassword']
+        reg = NEWUSER.create(email, username, password, cpassword)
+        if reg == 1:
+            flash('Registration Successfull.Proceed to Login')
+            return redirect(url_for('login'))
+        error = reg
+        return redirect(url_for('register'))
     return render_template('signup.html', form=form)
