2862d2709c92badf323037e5ab1019b0ded036fd
Stephen Muthama <muthamass@gmail.com>
2017-10-11 22:26:24 +0300

    updates user __init__

--- app/user.py
+++ app/user.py
@@ -8,14 +8,13 @@ class User():
     """
     User class that contains the methods to craete a user and login the user
     """
-    def __init__(self, username=None, email=None, password=None, cpassword=None):
+    def __init__(self, username=None, email=None, password=None):
         """
         Initialization of the user class
         """
         self.username = username
         self.email = email
         self.password = password
-        self.cpassword = cpassword
 
     def create(self, email, username, password, cpassword):
         """ Method for creating a user"""
@@ -30,22 +29,24 @@ class User():
                     len(newpass) == len(password) and
                     len(newcpass) == len(cpassword)
             ):
-                regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)"
-                if re.search(regex, email):
-                    namelength = len(username)
-                    if namelength >= 6 and namelength <= 25:
-                        passlength = len(password)
-                        if passlength >= 6 and passlength <= 25:
-                            if email not in USERS.keys():
-                                if password == cpassword:
-                                    USERS[email] = [username, password]
-                                    result = email in USERS.keys()
-                                    return result
+                emaillength = len(email)
+                if emaillength >= 6 and emaillength <= 50:
+                    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)"
+                    if re.search(regex, email):
+                        namelength = len(username)
+                        if namelength >= 6 and namelength <= 25:
+                            passlength = len(password)
+                            if passlength >= 6 and passlength <= 25:
+                                if email not in USERS.keys():
+                                    if password == cpassword:
+                                        USERS[email] = [username, password]
+                                        result = email in USERS.keys()
+                                        return result
+                                    return 8
                                 return 7
                             return 6
                         return 5
-                    return 4    
+                    return 4
                 return 3
             return 2
         return 1
-
