e759f8319e7318adecb9e30deb622576ba9b7f35
Stephen Muthama <muthamass@gmail.com>
2017-10-11 12:59:27 +0300

    updates routes for login and index

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
-from flask import render_template
+from flask import render_template, url_for
 
 from app import app
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route('/register')
 def register():
     return render_template("signup.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
