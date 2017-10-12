a83741817d9685072e74b58db2030a708816b710
Stephen Muthama <muthamass@gmail.com>
2017-10-12 20:01:40 +0300

    positions return correctly

--- app/category.py
+++ app/category.py
@@ -7,28 +7,32 @@ class Category():
     """
     Category class that contains the methods to craete, View, Update, Delete categories
     """
-    def __init__(self, catname=None, email=None, catid=None):
+    def __init__(self):
         """
         Initialization of the category class
         """
-        self.catname = catname
-        self.email = email
-        self.catid = catid
+        self.catname = None
+        self.email = None
+        self.catid = None
 
     def create_category(self, catname, email):
         """ Method for creating a categoris"""
         size = len(CATS)
+        self.catname = catname
+        self.email = email
         self.catid = size
-        return CATS.insert(self.catid, {'catname': catname, 'email': email, 'catid': size})
+        CATS.insert(self.catid, {'catname': self.catname, 'email': self.email, 'catid': self.catid})
+        return CATS
 
     def view_category(self, email):
         """View method to view the categories"""
         mycats = []
+        self.email = email
         for listvalue in CATS:
             for key in listvalue:
-                if listvalue[key] == email:
+                if listvalue[key] == self.email:
                     mycats.append(listvalue)
-                    return mycats
+        return mycats
 
     @staticmethod
     def delete_category(catid):
