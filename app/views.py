a0cc9fbafba2e8c46ad9e754a101850cab9d4f27
Stephen Muthama <muthamass@gmail.com>
2017-10-12 03:14:45 +0300

    adds login POST method

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, redirect, request, flash
 
 from app import app
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, LoginForm
 from app.user import User
 
 NEWUSER = User()
@@ -14,9 +14,21 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        # if form.validate_on_submit():
+        email = 'mwangi@mwangi.com'
+        password = 'mypassword'
+        reg = NEWUSER.login(email, password)
+        if reg == 1:
+            flash('Welcome')
+            return redirect(url_for('index'))
+        else:
+            error = reg
+            return render_template('login.html', form=form, error=error)
+    return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -30,7 +42,7 @@ def register():
         cpassword = 'mypassword'
         reg = NEWUSER.create(email, username, password, cpassword)
         if reg == 1:
-            flash('Registration Successfull.Proceed to Login')
+            flash('Registration Successfull')
             return redirect(url_for('login'))
         else:
             error = reg
