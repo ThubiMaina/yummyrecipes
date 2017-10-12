f8384afc2d334cd6e97ad13194adce5ba6294cea
Stephen Muthama <muthamass@gmail.com>
2017-10-12 03:46:09 +0300

    creates categories

--- app/category.py
+++ app/category.py
@@ -9,10 +9,14 @@ class Category():
     """
     def __init__(self, catname=None, email=None, catid=0):
         """
-        Initialization of the user class
+        Initialization of the category class
         """
         self.catname = catname
         self.email = email
         self.catid = catid
 
-    
+    def create(self, catname, email):
+        """ Method for creating a categoris"""
+        size = len(CATS)
+        CATS.insert(size, {'catname': catname, 'email': email, 'catid': size})
+
