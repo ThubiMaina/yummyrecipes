5a11582198c3e6c99ddc4dad5fbb7ce7fed05a60
Stephen Muthama <muthamass@gmail.com>
2017-10-15 22:44:17 +0300

    tests for category field valid input

--- tests/test_category.py
+++ tests/test_category.py
@@ -7,6 +7,24 @@ class TestCategory(unittest.TestCase):
     """class to test user case"""
     def setUp(self):
         self.category = Category()
-        self.catname = None
-        self.email = None
+        self.catname = 'First Category'
+        self.email = 'muthamass@gmail.com'
         self.catid = None
+
+    def test_create_category(self):
+        """ method to test for success in creating user account"""
+        new_category = self.category.create_category(self.catname, self.email)
+        self.assertEqual(1, new_category, "Category Not Created")
+
+    def test_category_name_is_not_blank(self):
+        """ method to test if username field is empty"""
+        new_category = self.category.create_category(self.catname, self.email)
+        self.assertEqual("Category name cannot be blank.", new_category)
+
+    def test_category_name_is_valid_length(self):
+        """ method to test if username is valid length"""
+        long_name = ''.join(random.choice(string.ascii_lowercase) for _ in range(31))
+        invalid_names = [long_name, 'short']
+        for catname in invalid_names:
+            new_category = self.category.create_category(catname, self.email)
+            self.assertEqual("Field must be between 6 and 30 characters long.", new_category)
