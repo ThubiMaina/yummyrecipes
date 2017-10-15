773f567df09b4e563444a73aa38c152a12edb32b
Stephen Muthama <muthamass@gmail.com>
2017-10-15 16:16:55 +0300

    test cases for user test class

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,74 +7,38 @@ from app.user import User
 class TestUser(unittest.TestCase):
     """class to test user case"""
     def setUp(self):
-        self.new_user = User()
-        
-        """Creates account successfully,
-        check user email available
-        inputs blank, Individual field blank-4
-        inputs starts with anything other than letter-4
-        input has trailling white spaces-4
-        password matching.
-        invalid email
-        min 6.
-        max length
-        """
-    def test_all_inputs_are_empty(self):
-        """ method to test if input fields are empty"""
-        result = self.new_user.create('', '', '', '')
-        self.assertEqual(1, result, "Input fields empty")
-    
-    def test_email_is_empty(self):
-        """ method to test if email field is empty"""
-        result = self.new_user.create('', 'Peter', 'patpass', 'patpass')
-        self.assertEqual(1, result, "Input fields empty")
-
-    def test_username_is_empty(self):
-        """ method to test if username field is empty"""
-        result = self.new_user.create('maua@mail.com', '', 'plutos', 'plutos')
-        self.assertEqual(1, result, "Username empty")
-    
-    def test_password_is_empty(self):
-        """ method to test if password field is empty"""
-        result = self.new_user.create('maua@mail.com', 'Earths', '', 'plutos')
-        self.assertEqual(1, result, "Password empty")
-    
-    def test_cpassword_is_empty(self):
-        """ method to test if cpassword field is empty"""
-        result = self.new_user.create('maua@mail.com', 'Earths', 'plutos', '')
-        self.assertEqual(1, result, "Confirm Password empty")
-    
-    def test_trailing_whitespace(self):
-        """ method to test if inputs have trailing whitespaces"""
-        result = self.new_user.create(' maua@mail.com ', ' Earths ', ' plutos ', ' plutos ')
-        self.assertEqual(2, result, "Trailing Whitespaces available")
-
-    def test_email_trailing_whitespace(self):
-        """ method to test if email has trailing whitespaces"""
-        result = self.new_user.create('maua@mail.com ', ' Earths ', 'plutos ', 'plutos ')
-        self.assertEqual(2, result, "Username has trailing Whitespaces")
-
-    def test_username_has_trailing_whitespace(self):
-        """ method to test if password has trailing whitespaces"""
-        result = self.new_user.create('maua@mail.com ', ' Earths ', 'plutos ', 'plutos ')
-        self.assertEqual(2, result, "Username has trailing Whitespaces")
-
-    def test_password_trailing_whitespace(self):
-        """ method to test if email has trailing whitespaces"""
-        result = self.new_user.create(' maua@mail.com ', 'Earths ', ' plutos ', 'plutos ')
-        self.assertEqual(2, result, "Password has trailing Whitespaces")
-
-    def test_cpassword_trailing_whitespace(self):
-        """ method to test if email has trailing whitespaces"""
-        result = self.new_user.create(' maua@mail.com ', 'Earths ', 'plutos ', ' plutos ')
-        self.assertEqual(2, result, "Cpassword has trailing Whitespaces")        
-
-        
+        self.user = User()
+        self.username = 'Muthamass'
+        self.email = 'muthamass@gmail.com'
+        self.password = 'password'
+        self.cpassword = 'password'
 
     def test_create_user(self):
         """ method to test for success in creating user account"""
-        user_details = self.new_user.create(
-            'maua@gmail.com', 'Peter', 'patpass', 'patpass')
-        self.assertTrue(user_details, "User succesfully created")
+        new_user = self.user.create(self.email, self.username, self.password, self.cpassword)
+        self.assertEqual(1, new_user, "User Not Created")
     
-    
+    def test_user_exists(self):
+        """ method to test if user exists in the database"""
+        result = self.user.check_email_in_db(self.email)
+        self.assertEqual(1, result, "User exists")
+
+    def test_email_is_blank(self):
+        """ method to test if email field has been left blank"""
+        new_user = self.user.create('', self.username, self.password, self.cpassword)
+        self.assertEqual("Email should not be blank", new_user)
+
+    def test_email_contains_trailing_spaces(self):
+        """ method to test if email contains trailing spaces"""
+        new_user = self.user.create(' muthamass@gmail.com ', self.username, self.password, self.cpassword)
+        self.assertEqual("Email cannot have blank space or tabs", new_user)
+
+    def test_email_is_valid_format(self):
+        """ method to test if email address is valid"""
+        invalid_emails = ['mutha mass@gmail.com', 'muthamass @gmail.com',
+                          'muthamass@gmail', 'muthamass@gmail.',
+                          'muthamass@ gmail.com', 'muthamass@gm ail.com'
+                          'muthamass@gmail. com', 'muthamass@gmail.c om']
+        for email in invalid_emails:
+            new_user = self.user.create(email, self.username, self.password, self.cpassword)
+            self.assertEqual("Enter a valid email address", new_user)
