db033c97c66db90e728e09110b4c95b0ab87a756
Stephen Muthama <muthamass@gmail.com>
2017-10-15 23:11:43 +0300

    adds category create method

--- app/category.py
+++ app/category.py
@@ -1,4 +1,6 @@
 """ create category class to enable create, view and delete of categories"""
+import re
+
 
 CATS = []
 
@@ -18,42 +20,30 @@ class Category():
 
     def create_category(self, catname, email):
         """ Method for creating a categoris"""
-        size = len(CATS)
-        self.catname = catname
-        self.email = email
-        self.catid = size
-        CATS.insert(self.catid, {'catname': self.catname, 'email': self.email, 'catid': self.catid})
-        return CATS
-
-    def view_category(self, email):
-        """View method to view the categories"""
-        mycats = []
-        self.email = email
-        for listvalue in CATS:
-            for key in listvalue:
-                if listvalue[key] == self.email:
-                    mycats.append(listvalue)
-        return mycats
+        self.catname = self.check_category_name(catname)
+        if self.catname == 1:
+            size = len(CATS)
+            self.email = email
+            self.catid = size
+            CATS.insert(self.catid, {'catname': self.catname, 'email': self.email, 'catid': self.catid})
+            new_category = CATS[self.catid]
+            result = new_category['catname']
+            if result == self.catname:
+                return 1
+            message = "Failed to create category"
+            return message
+        return self.catname
 
     @staticmethod
-    def delete_category(catid):
-        """Delete the category from the global list"""
-        del CATS[catid]
-        CATS.insert(catid, {'catname': 'deleted', 'email': 'del@del.com', 'catid': catid})
-        return CATS
-
-    def get_category_name(self, catid):
-        mycat = CATS[catid]
-        for key in mycat:
-            if key == 'catname':
-                catname = mycat[key] 
-                return catname
-
-    def update_category(self, catname, email, catid):
-        """Update the category with the new inputs"""
-        self.catname = catname
-        self.email = email
-        self.catid = catid
-        del CATS[self.catid]
-        CATS.insert(self.catid, {'catname': self.catname, 'email': self.email, 'catid': self.catid})
-        return CATS
+    def check_category_name(category_name):
+        """ Method to check if category name entered is valid """
+        new_category_name = category_name.strip()
+        if new_category_name != '':
+            catname = re.sub(r'\s+', ' ', new_category_name)
+            name_length = len(catname)
+            if name_length >= 6 and name_length <= 30:
+                return 1
+            message = "Field must be between 6 and 30 characters long."
+            return message
+        message = "Category name cannot be blank."
+        return message
