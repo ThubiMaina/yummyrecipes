1c127f0936f1f6de6038b675c606626f3ec35bb5
Stephen Muthama <muthamass@gmail.com>
2017-10-16 02:35:09 +0300

    updates category update mathod

--- app/category.py
+++ app/category.py
@@ -19,19 +19,23 @@ class Category():
 
     def create_category(self, catname, email):
         """ Method for creating a categories"""
-        self.catname = self.check_category_name(catname)
-        if self.catname == 1:
-            size = len(CATS)
-            self.email = email
-            self.catid = size
-            CATS.insert(self.catid, {'catname': self.catname, 'email': self.email, 'catid': self.catid})
-            new_category = CATS[self.catid]
-            result = new_category['catname']
-            if result == self.catname:
-                return 1
-            message = "Failed to create category"
-            return message
-        return self.catname
+        valid_catname = self.check_category_name(catname)
+        if valid_catname != "Field must be between 6 and 30 characters long.":
+            if valid_catname != "Category name cannot be blank.":
+                size = len(CATS)
+                self.catname = valid_catname
+                self.email = email
+                self.catid = size
+                CATS.insert(self.catid, {'catname': self.catname, 
+                                         'email': self.email, 'catid': self.catid})
+                new_category = CATS[self.catid]
+                store_catname = new_category['catname']
+                if store_catname == self.catname:
+                    return 1
+                message = "Failed to create category"
+                return message
+            return valid_catname
+        return valid_catname
 
     def view_category(self, email):
         """Method to view the categories using the session email address"""
@@ -48,7 +52,7 @@ class Category():
         return message
 
     def delete_category(self, catid, email):
-        """Delete the category from the global list"""
+        """Method to delete the category name"""
         self.catid = int(catid)
         category_to_delete = CATS[self.catid]
         self.catname = category_to_delete['catname']
@@ -64,7 +68,36 @@ class Category():
                 return 1
             message = 'failed to delete'
             return message
-        message = "The action is Forbideen"
+        message = "The action is Forbiden"
+        return message
+
+    def update_category(self, catname, email, catid):
+        """Method to update the category name"""
+        size = len(CATS)
+        if catid < size:
+            store_category = CATS[catid]
+            store_catname = store_category['email']
+            valid_catname = self.check_category_name(catname)
+            if valid_catname != store_catname:
+                if valid_catname != "Field must be between 6 and 30 characters long.":
+                    if valid_catname != "Category name cannot be blank.":
+                        self.catname = valid_catname
+                        self.email = email
+                        self.catid = int(catid)
+                        CATS.insert(self.catid, {'catname': self.catname,
+                                                'email': self.email,
+                                                'catid': self.catid})
+                        updated_category = CATS[self.catid]
+                        updated_catname = updated_category['catname']
+                        if updated_catname == self.catname:
+                            return 1
+                        message = "Category Name"
+                        return message
+                    return valid_catname
+                return valid_catname
+            message = "The name is still the same"
+            return message
+        message = "Forbiden!Category does not exist"
         return message
 
     @staticmethod
@@ -75,7 +108,8 @@ class Category():
             catname = re.sub(r'\s+', ' ', new_category_name)
             name_length = len(catname)
             if name_length >= 6 and name_length <= 30:
-                return 1
+                valid_catname = catname
+                return valid_catname
             message = "Field must be between 6 and 30 characters long."
             return message
         message = "Category name cannot be blank."
