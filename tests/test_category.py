6aa9f344968eac3e0117fdad6e56b06ecef96cb5
Stephen Muthama <muthamass@gmail.com>
2017-10-16 00:32:46 +0300

    tests category delete method

--- tests/test_category.py
+++ tests/test_category.py
@@ -32,10 +32,24 @@ class TestCategory(unittest.TestCase):
             self.assertEqual("Field must be between 6 and 30 characters long.", new_category)
 
     def test_user_can_view_his_categories(self):
+        """ method to test if user can successfully see his categories"""
         view_category = self.category.view_category(self.email)
         self.assertEqual(1, view_category)
 
     def test_returns_empty_if_user_lacks_categories(self):
+        """ method to test if user can view another users category"""
         self.email = 'stephen@gmail.com'
         view_category = self.category.view_category(self.email)
         self.assertEqual("Create your first category here >>>", view_category)
+
+    def test_category_is_successfully_deleted(self):
+        """ method to test if user can delete their categories"""
+        self.category.create_category(self.catname, self.email)
+        category_to_delete = self.category.delete_category(0, self.email)
+        self.assertEqual(1, category_to_delete)
+
+    def test_user_can_delete_another_user_category(self):
+        """ method to test if user can delete another users category"""
+        self.category.create_category(self.catname, 'steve@gmail.com')
+        category_to_delete = self.category.delete_category(0, self.email)
+        self.assertEqual("The action is Forbideen", category_to_delete)
